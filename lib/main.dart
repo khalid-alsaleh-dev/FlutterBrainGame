ae53fadeb7f9c88630cace41bd75bb5969abdd88
khalid alsaleh <khalidlionel.2089@gmail.com>
2022-02-16 18:15:16 -0800

    change status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:brain_game/utils/game_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:brain_game/ui/screens/home.dart';
 import 'package:brain_game/utils/game_assets.dart';
+import 'package:flutter/services.dart';
 
 void main()async {
+   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor:GameColors.primary, 
+    statusBarColor: GameColors.primary, 
+  ));
   WidgetsFlutterBinding.ensureInitialized();
  await GameAssets.prereloadAssets();
   runApp(
