b24b14fbb02e5499589e747e14c1d6bf6c8e542b
Khalid Al-Saleh <67127338+khalid-alsaleh-dev@users.noreply.github.com>
2022-02-27 18:41:30 +0200

    Correct spelling mistakes

--- lib/classes/event_generator.dart
+++ lib/classes/event_generator.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'package:brain_game/enums/event.dart';
 
 class EventGenerator {
-  /// A function used generates a random event
+  /// A function generates a random event
   static Event generate() {
     int randomValue = Random(DateTime.now().millisecondsSinceEpoch).nextInt(105);
     if (randomValue <= 35) {
